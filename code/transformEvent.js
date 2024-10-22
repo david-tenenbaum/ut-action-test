ce6e9f084167607ab93f265f54e23e023bbd44a5
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro-2.local>
2024-10-22 14:49:11 -0400

    will this work

--- code/transformEvent.js
+++ code/transformEvent.js
@@ -1,4 +1,7 @@
-import { getTransformedEvent } from "getTransformedEvent";
+import { getTransformedEvent } from 'getTransformedEvent';
+
+import { PIPELINE_VERSION } from 'pipelineVersion';
+import { formatEvent } from 'formatEvent';
 
 export function transformEvent(event, metadata) {
   return getTransformedEvent(event);
